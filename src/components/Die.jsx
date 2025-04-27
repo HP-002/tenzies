65736cd41d067ea2bc76f628e7c8edcd0f3c7328
hetpatel <hetfaldu19@gmail.com>
2025-04-27 14:45:14 -0400

    Added dice and roll button functionalities

---
+++ src/components/Die.jsx
@@ -0,0 +1,7 @@
+import "../styles/die.css"
+
+export function Die(props) {
+    return (
+        <button>{props.value}</button>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,30 @@
+import { useState } from 'react'
+import { Die } from './Die.jsx'
 import "../styles/main.css"
 
 export function Main() {
+    const generateRandomDice = () => {
+        const dice = [];
+        for (let i = 0; i < 10; i++) {
+            dice.push(Math.floor(Math.random() * 6) + 1);
+        }
+        return dice;
+    }
+
+    const [dice, setDice] = useState(generateRandomDice());
+
+    const diceElements = dice.map(value => <Die value={value} />);
+
+    const updateDice = () => {
+        setDice(generateRandomDice());
+    }
+    
     return (
         <main className="main">
-            
+            <div className='dice-container'>
+                {diceElements}
+            </div>
+            <button className='roll-button' onClick={updateDice}>Roll</button>
         </main>
     )
 }
